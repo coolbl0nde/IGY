1eaaf2d238c409fd053d5e3c5a29b753a748cf72
marusya <meri@Ubuntu22.04.myguest.virtualbox.org>
2023-03-01 21:27:47 +0300

    Added function for getting even numbers from list

--- Lab1/utilities.py
+++ Lab1/utilities.py
@@ -1,6 +1,7 @@
 def print_hello():
     print('Hello world')
 
+
 def calculation(first_number, second_number, operation):
 
     if operation == "add":
@@ -14,4 +15,12 @@ def calculation(first_number, second_number, operation):
     else:
         print("Incorrect operation!")
 
-        
+
+def get_even_numbers(numbers):
+
+    result = []
+    for number in numbers:
+        if not number % 2:
+            result.append(number)
+
+    return result
