309eaf49810ba29fc6b86bfb1247f6d96d2baf1b
marusya <meri@Ubuntu22.04.myguest.virtualbox.org>
2023-03-01 21:23:56 +0300

    Added function for calculation math operations between 2 numbers

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,3 +1,4 @@
 from utilities import *
 
 print_hello()
+print(calculation(3, 5, "mult"))
--- Lab1/utilities.py
+++ Lab1/utilities.py
@@ -1,2 +1,17 @@
 def print_hello():
     print('Hello world')
+
+def calculation(first_number, second_number, operation):
+
+    if operation == "add":
+        return first_number + second_number
+    elif operation == "sub":
+        return first_number - second_number
+    elif operation == "mult":
+        return first_number * second_number
+    elif operation == "div":
+        return first_number / second_number
+    else:
+        print("Incorrect operation!")
+
+        
