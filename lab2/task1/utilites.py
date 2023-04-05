59d3486508d4d05a9d1de396c047c843b12c6f4c
marusya <meri@Ubuntu22.04.myguest.virtualbox.org>
2023-04-06 02:32:16 +0300

    add functions sentences_length and words_length

--- lab2/task1/utilites.py
+++ lab2/task1/utilites.py
@@ -26,3 +26,41 @@ def amount_of_non_declarative_sentences(text):
 
     print(amount)
     return amount
+
+
+def to_words(text):
+    reg = r'\s*([^.!?]+)*[.!?]'
+    sentences = re.findall(reg, text)
+
+    words = []
+    reg = r'\b\w*[a-zA-Z]\w*\b'
+
+    for sentence in sentences:
+        words += re.findall(reg, sentence)
+
+    print(words)
+    return words
+
+
+def characters_in_text(text):
+    words = to_words(text)
+    amount = 0
+
+    for word in words:
+        amount += len(word)
+
+    return amount
+
+
+def sentences_length(text):
+    amount = amount_of_sentences(text) + amount_of_non_declarative_sentences(text)
+    characters = characters_in_text(text)
+
+    return characters/amount
+
+
+def words_length(text):
+    amount = len(to_words(text))
+    characters = characters_in_text(text)
+
+    return characters/amount
